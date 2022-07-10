06efa7f4729c37ac06863b628af391701b65403c
Seogba <101324621+Seogba@users.noreply.github.com>
2022-07-10 23:59:11 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function App() {
+function MinutesToHours() {
   const [amount, setAmount] = useState(0);
   const [flipped, setFlipped] = useState(false);
   const onChange = (event) => {
@@ -18,7 +18,6 @@ function App() {
   return (
     <div>
       <div>
-        <h1>Super Converter</h1>
         <label htmlFor="minutes">Minutes</label>
         <input
           value={flipped ? amount * 60 : amount}
@@ -43,6 +42,30 @@ function App() {
       </div>
       <button onClick={reset}>Nuclear</button>
       <button onClick={onFlip}>Flip</button>
+    </div>
+  );
+}
+
+function KmToMiles() {
+  return <h3>KM 2 M</h3>;
+}
+
+function App() {
+  const [index, setIndex] = useState("xx");
+  const onSelect = (event) => {
+    setIndex(event.target.value);
+  };
+  return (
+    <div>
+      <h1>Super Converter</h1>
+      <select value={index} onChange={onSelect}>
+        <option value="xx">Select Your units</option>
+        <option value="0">Minutes & Hours</option>
+        <option value="1">Km & Miles</option>
+      </select>
+      <hr />
+      {index === "0" ? <MinutesToHours /> : null}
+      {index === "1" ? <KmToMiles /> : null}
     </div>
   );
 }
