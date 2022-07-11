90defc48ed844d165ef1fcbacd37c2c5e305533c
Seogba <101324621+Seogba@users.noreply.github.com>
2022-07-11 22:22:06 +0900

    Add files via upload

--- App.js
+++ App.js
@@ -2,70 +2,31 @@ import logo from "./logo.svg";
 import "./App.css";
 import { useState, useEffect } from "react";
 
-function MinutesToHours() {
-  const [amount, setAmount] = useState(0);
-  const [flipped, setFlipped] = useState(false);
-  const onChange = (event) => {
-    setAmount(event.target.value);
-  };
-  const reset = () => {
-    setAmount(0);
-  };
-  const onFlip = () => {
-    reset();
-    setFlipped((current) => !current);
-  };
+function Btn({ text, changeValue }) {
+  console.log(text, "was rendered");
   return (
-    <div>
-      <div>
-        <label htmlFor="minutes">Minutes</label>
-        <input
-          value={flipped ? amount * 60 : amount}
-          id="minutes"
-          type="number"
-          onChange={onChange}
-          disabled={flipped}
-        />
-      </div>
-
-      <div>
-        <h4>You want to convert {amount}</h4>
-        <label htmlFor="hours">Hours</label>
-        <input
-          value={flipped ? amount : Math.round(amount / 60)}
-          id="hours"
-          placeholder="hours"
-          type="number"
-          disabled={!flipped}
-          onChange={onChange}
-        />
-      </div>
-      <button onClick={reset}>Nuclear</button>
-      <button onClick={onFlip}>Flip</button>
-    </div>
+    <button
+      onClick={changeValue}
+      style={{
+        backgroundColor: "pink",
+        color: "white",
+        padding: "10px 20px",
+        border: 0,
+        borderRadius: 10,
+      }}
+    >
+      {text}
+    </button>
   );
 }
 
-function KmToMiles() {
-  return <h3>KM 2 M</h3>;
-}
-
 function App() {
-  const [index, setIndex] = useState("xx");
-  const onSelect = (event) => {
-    setIndex(event.target.value);
-  };
+  const [value, setValue] = useState("Save Changes");
+  const changeValue = () => setValue("Revert Changes");
   return (
     <div>
-      <h1>Super Converter</h1>
-      <select value={index} onChange={onSelect}>
-        <option value="xx">Select Your units</option>
-        <option value="0">Minutes & Hours</option>
-        <option value="1">Km & Miles</option>
-      </select>
-      <hr />
-      {index === "0" ? <MinutesToHours /> : null}
-      {index === "1" ? <KmToMiles /> : null}
+      <Btn text={value} changeValue={changeValue} />
+      <Btn text="Continue" />
     </div>
   );
 }
